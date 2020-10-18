0f179171dd94587c56af692ed7c761e9240688c4
qor0530 <qorwhdghk12@gmail.com>
2020-10-18 16:24:36 +0900

    1. 캐릭터, 총알 생성 2. 총알의 콜라이더 설정

---
+++ Topview_shootingGame/Assets/Script/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(0, 0.1f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Wall")
+        {
+            Destroy(gameObject);
+        }
+        //else if(collision.gameObject.name == "Monster")
+        //{
+        //    //
+        //    Destroy(gameObject);
+        //}
+    }
+}
---
+++ Topview_shootingGame/Assets/Script/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bullet;
+
+    private int bulletTerm = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + new Vector3(-0.1f, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(0.1f, 0);
+        }
+        if(bulletTerm++ >= 20)
+        {
+            var bulletObject = Instantiate(bullet);
+            bulletObject.transform.position = transform.position;
+            bulletTerm = 0;
+        }
+    }
+}
